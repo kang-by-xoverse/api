2f25403127a9a6cbdd6f6c579838b693ea5dab6b
Nie-Mand <chill.out.x19@gmail.com>
2022-11-26 16:07:34 +0100

    placeholder to simulate an event

--- rest/main.go
+++ rest/main.go
@@ -3,11 +3,14 @@ package rest
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
+
+	"kang-by-xoverse/api/utils"
 )
 
 func RunRestServer(rdb *redis.Client) {
 	r := gin.Default()
 	r.GET("/api/ping", func(c *gin.Context) {
+		go utils.Emit(rdb, "META_CREATED", "{\"variant\": \"red\"}")
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
